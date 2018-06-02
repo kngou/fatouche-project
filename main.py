2a2a44c715ee1f54cc31fdff5e95cae5e9081123
kngou <39131830+kngou@users.noreply.github.com>
2018-06-02 02:23:18 +0200

    minor changes
    readcomandline module; changed the option.arg to args because the first one is depreciated. 
    Changes in parsing input files

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ This module is the main module
 """
 # -- read input command line
 
-folderName,master,genomicFile,cdnaFile,verbose = readCommandLine.commandLine()
+folderName,master,genomicList,cdnaList,verbose = readCommandLine.commandLine()
+# print(folderName,master,genomicFile,cdnaFile,verbose )
 
 
+# sys.exit()
 # -- Creation of result folder by default the folder name is "result" but it can be changed thanks to the command line
 #    it will contain all output files of this program but circos related files 
 
@@ -39,7 +41,7 @@ inFile_related_function.createResultFolder(folderName)
 				*genomicName and cdnaName retrieve thanks to -gF -cF or cdnaFile and genomicFile
 
 """
-if master and  cdnaFile or  genomicFile:
+if (master and  cdnaList) or (master and genomicList):
 	print ("You can not choose these options together, please press -h to know the correct option.")
 	sys.exit()
 elif master:
@@ -49,20 +51,39 @@ elif master:
 	for file in file :
 		genomicFile = file
 		cdnaFile = file.replace("fasta","fa")	
-		outExon = function.runMain(genomicFile,cdnaFile,folderName) # output of exonlist from genomic and cdna files.
+		function.runMain(genomicFile,cdnaFile,folderName) # output of exonlist from genomic and cdna files.
 		allFileIn+=1
-	print (allFileIn)
+
 	if NumOfFile == allFileIn:
 		genomicReport,cdnaReport = outFile_related.generateReport(folderName) # outputs final report with amorce list
-elif genomicFile and cdnaFile:
-	outExon = function.runMain(genomicFile,cdnaFile,folderName)  # output of exonlist from genomic and cdna files.
-	if outExon:
-		genomicReport,cdnaReport = outFile_related.generateReport(folderName) # outputs final report with amorce list
-	else: 
-		print("something went wrong with output files!")
+elif genomicList and cdnaList:	
+	allFileIn = 0
+	genomicFile = str(genomicList).split(",")
+	cdnaFile = str(cdnaList).split(",")
+	genoSize = len(genomicFile)
+	cdnaSize = len(cdnaFile)	
+	if genoSize != cdnaSize:
+		print("something went wrong with input files!")
+		sys.exit()
+	elif genoSize == cdnaSize:
+		for index in range(genoSize):
+			genomic = genomicFile[index].replace("[\'","")
+			genomic= genomic.replace('\']','')
+			cdna = cdnaFile[index].replace('[\'','')
+			cdna = cdna.replace('\']','')
+			# print(cdna,genomic,folderName) # output of exonlist from genomic and cdna files.
+			function.runMain(genomic,cdna,folderName) # output of exonlist from genomic and cdna files.
+		
+		outfiles = glob.glob(".\\"+folderName+"\\*.fa_exon_list.txt")
+		if len (outfiles) == genoSize:
+			genomicReport,cdnaReport = outFile_related.generateReport(folderName) # outputs final report with amorce list
+		else: 
+			print("something went wrong with output files!")
 if verbose :
-	print("these files have been created :",genomicReport,"\n",cdnaReport, "\nhave a nice day !")
+	print("these files have been created :\n",genomicReport,"\n",cdnaReport, "\nhave a nice day !")
 sys.exit()
+
+
 
 
 
--- readCommandLine.py
+++ readCommandLine.py
@@ -3,30 +3,37 @@
 import os
 import re
 import sys
-import getopt
+import argparse 
 
 
-# getopt.getopt(args, options, [long_options])
-# def main(argv):
-#    inputfile = ''
-#    outputfile = ''
-#    try:
-#       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-#    except getopt.GetoptError:
-#       print 'test.py -i <inputfile> -o <outputfile>'
-#       sys.exit(2)
-#    for opt, arg in opts:
-#       if opt == '-h':
-#          print 'test.py -i <inputfile> -o <outputfile>'
-#          sys.exit()
-#       elif opt in ("-i", "--ifile"):
-#          inputfile = arg
-#       elif opt in ("-o", "--ofile"):
-#          outputfile = arg
-#    print 'Input file is "', inputfile
-#    print 'Output file is "', outputfile
 
-# if __name__ == "__main__":
-#    main(sys.argv[1:])
+def commandLine():
+	folderName = ""
+	master = ""
+	genomicFile = ""
+	cdnaFile = ""
+	verbose = ""
+	
+	parser = argparse.ArgumentParser()
 
-# """"
+	parser.add_argument("-m", "--master", action ="store_true",default = False, dest= "master", 
+						help = "It searches on the current folder all files with fasta extension and it uses it as values for genomic file name. Use it if your current folder contains only the fasta files that you need. Pay attention, that the corresponding cdna file must have fa as extension unless you will retrieve an error message")
+	parser.add_argument("-g", "--genomicFile ",
+	                   action ="store", type =str, dest= "genomicFile", nargs ="*",	                   
+	                   help ="genomic file name to input. If you input a list of files, file names must be separated by a coma.")
+	parser.add_argument("-v", "--verbose",
+	                   action ="store_true", dest="verbose", default = False,
+	                   help ="choose this option if you want to output a message for each step.")
+	parser.add_argument("-c", "--cdnaFile ",
+	                  action ="store", type =str, dest = "cdnaFile", nargs ="*",
+	                  help ="cdna file name list to input. Each file name must be separated by a coma. Files must be in the same order as its corresponding genomic file, or it must have the same name.")
+	parser.add_argument("-f", "--folderName ",action ="store", type =str, dest = "folderName", default="result",
+					  help ="folder name for ouput files")
+	options = parser.parse_args()
+	master = options.master
+	verbose = options.verbose
+	folderName = options.folderName
+	# -- input file names are stored on list each file is separated by a coma 
+	genomicList = options.genomicFile
+	cdnaList = options.cdnaFile	
+	return folderName,master,genomicList,cdnaList,verbose
